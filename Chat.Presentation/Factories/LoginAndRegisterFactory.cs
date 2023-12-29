d2d112c20c8425a16849e1a540548efab44c49d9
Tin922 <tinjuric92@gmail.com>
2023-12-29 22:04:20 +0100

    work on factories in presentation layer

--- Chat.Presentation/Factories/LoginAndRegisterFactory.cs
+++ Chat.Presentation/Factories/LoginAndRegisterFactory.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace Chat.Presentation.Factories
 {
-    internal class LoginAndRegisterFactory
+    public class LoginAndRegisterFactory
     {
+
     }
 }
